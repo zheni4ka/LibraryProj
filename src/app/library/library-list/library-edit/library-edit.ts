edfd5ab1f4380d6bc69709253f816465f2adc86a
vo1ce <vo1ce@DESKTOP-ARNJTR2>
2024-04-07 21:52:45 +0300

    123

--- src/app/library/library-list/library-edit/library-edit.ts
+++ src/app/library/library-list/library-edit/library-edit.ts
@@ -37,7 +37,45 @@ export class EditBookComponent
     constructor(private fb: FormBuilder,
         private service: LibraryService,
         private location: Location,
-        private route: ActivatedRoute){}
+        private route: ActivatedRoute)
+        {
+            this.form = this.fb.group({
+                id: [0, Validators.required],
+                name: ['', Validators.required],
+                description: ['', Validators.minLength(10)],
+                inStock: [false],
+                imageUrl: ['', Validators.required],
+                pageCount: [Validators.required, Validators.minLength(0)],
+              });
+        }
     
+        ngOnInit(): void {
+
+            this.id = Number(this.route.snapshot.paramMap.get('id'));
+        
+            this.service.get(this.id).subscribe(res => {
+              console.log(res);
+        
+              this.product = res;
+              this.form.setValue(res);
+            });
+          }
+        
+          onSubmit(): void {
+            if (!this.form.valid) return;
+        
+            const item = this.form.value as BookModel;
+            this.service.edit(item).subscribe();
+          }
+        
+          back(): void {
+            this.location.back();
+          }
+        
+          onCancel() {
+            if (this.product)
+              this.form.setValue(this.product);
+          }
+           
     
 }
